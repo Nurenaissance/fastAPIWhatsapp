926ac7d3caa43dde99e4f6a0e6b1fa27d4fdbe40
Shreyas9548 <shreyasv828@gmail.com>
2024-11-18 09:00:04 +0530

    added /update-contact

--- contacts/router.py
+++ contacts/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Request, Depends ,HTTPException
+from fastapi import APIRouter, Request, Depends ,HTTPException, responses
 from sqlalchemy import orm
 from config.database import get_db
 from .models import Contact
@@ -25,3 +25,37 @@ def read_contacts(request: Request, db: orm.Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="No contacts found for this tenant")
 
     return contacts
+
+@router.patch("/contacts/")
+async def update_contact(request: Request, db: orm.Session = Depends(get_db)):
+    body = await request.json()  # Parse JSON request body
+    contacts = body.get('contact_id', [])
+    bg_id = body.get('bgId')
+    bg_name = body.get('name')
+    errors = []
+
+    for contact_id in contacts:
+        try:
+            contact = db.query(Contact).filter(Contact.id == contact_id).first()
+            if not contact:
+                errors.append(f"Contact with id {contact_id} does not exist.")
+                continue
+
+            contact.bg_id = bg_id
+            contact.bg_name = bg_name
+
+            db.add(contact)
+            db.commit()
+
+
+        except Exception as e:
+            db.rollback()
+            errors.append(f"Error updating contact {contact_id}: {str(e)}")
+
+    if errors:
+        raise HTTPException(500, detail=errors)
+    else:
+        return responses.JSONResponse(content=None, status_code=200)
+        
+    
+        
