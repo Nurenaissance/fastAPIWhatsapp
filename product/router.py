e826c25aca6de4ab0d3588f093842ec6ed1c7702
Shreyas9548 <shreyasv828@gmail.com>
2024-11-23 09:28:06 +0530

    catalog/product-id

--- product/router.py
+++ product/router.py
@@ -15,3 +15,14 @@ def get_catalog(x_tenant_id: Optional[str] = Header(None), db: orm.Session = Dep
     except Exception as e:
         print("Exception occured in catalog: ", str(e))
         return HTTPException(500, detail=f"An Exception occured in catalog: {str(e)}")
+
+@router.get("/catalog/{product_id}/")
+def get_product(product_id = str ,x_tenant_id: Optional[str] = Header(None), db: orm.Session = Depends(get_db)):
+    try:
+        print("Catalog and Tenant ID rcd: ", product_id, x_tenant_id)
+        product  = db.query(Product).filter(Product.tenant_id == x_tenant_id, Product.product_id == product_id).first()
+
+        return product
+    except Exception as e:
+        print("Exception occured in catalog: ", str(e))
+        return HTTPException(500, detail=f"An Exception occured in catalog: {str(e)}")
