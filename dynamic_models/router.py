1fa122cd445d41697fd4dbf0e7814bb986300185
Shreyas9548 <shreyasv828@gmail.com>
2024-11-26 16:59:13 +0530

    fixed dynamic fields

--- dynamic_models/router.py
+++ dynamic_models/router.py
@@ -9,11 +9,16 @@ router = APIRouter()
 @router.get("/dynamic-models/")
 def get_dynamic_model( request: Request , db: orm.Session = Depends(get_db)):
     response_data =[]
+    tenant_id = request.headers.get('X-Tenant-Id')
+    # print(tenant_id)
+    
+    dynamic_models = db.query(DynamicModel).filter(DynamicModel.tenant_id == tenant_id).all()
 
-    dynamic_models = db.query(DynamicModel).all()
+    # print(dynamic_models)
     for dynamic_model in dynamic_models:
         try:
-            fields = db.query(DynamicField).filter(DynamicField.dynamic_model_id ==dynamic_model.id).all()
+            # print(dynamic_model.id)
+            fields = db.query(DynamicField).filter(DynamicField.dynamic_model_id == dynamic_model.id).all()
             fields_data = [{'field_name': field.field_name , 'field_type': field.field_type} for field in fields]
 
             model_data = {
@@ -23,14 +28,14 @@ def get_dynamic_model( request: Request , db: orm.Session = Depends(get_db)):
             response_data.append(model_data)
         except Exception as e:
             return HTTPException(500, detail=f"Exception occured in get-dynamic-model: {str(e)}")
-    
+    # print("Response Data: ", response_data)
     return response_data
 
 @router.get("/dynamic-models/{model_name}/")
 def get_dynamic_model_data(model_name: str, db: orm.Session = Depends(get_db)):
     try:
         # print("Databsse Bind URL: ", db.bind)
-        table_name = "dynamic_entities_nuren_ai_marketing_flow"
+        table_name = f"dynamic_entities_{model_name}"
 
         metadata = MetaData()
         table = Table(table_name, metadata, autoload_with=db.bind)
