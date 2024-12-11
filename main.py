6c441dede201756d52c48edb64e4f3ff7d63bf49
Shreyas9548 <shreyasv828@gmail.com>
2024-12-11 12:05:11 +0530

    add_emaail

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from config.database import engine, Base
 from config.middleware import add_cors_middleware
 import contacts.router, node_templates.router, scheduled_events.router, whatsapp_tenant.router
 import product.router, dynamic_models.router
-import conversations.router
+import conversations.router, emails
 
 newEvent = False
 app = FastAPI()
@@ -20,7 +20,7 @@ app.include_router(scheduled_events.router.router)
 app.include_router(product.router.router)
 app.include_router(dynamic_models.router.router)
 app.include_router(conversations.router.router)
-
+app.include_router(emails.router)
 
 @app.get("/")
 def read_root():
